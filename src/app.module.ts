aedbf0e5db54591a860b464bffec69be758e7c58
Canoral <kaya68@outlook.com>
2024-01-09 16:16:49 +0100

    Mise en place de la configuration de dotenv & connexion de la base de donnée

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'commons/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [config], isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config().database.host,
+      port: config().database.port,
+      username: config().database.username,
+      password: config().database.password,
+      database: config().database.database,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
